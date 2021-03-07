9a0ad9dc99706aefbb7f8451170e30092dc949e8
Veena-02 <veenachaddha1602@outlook.com>
2021-03-07 22:49:11 +0530

    node express morgan for static html pages

--- node-express/index.js
+++ node-express/index.js
@@ -1,8 +1,22 @@
 const http=require('http');
 const express=require('express');
+const morgan=require('morgan');
 
 const hostname='localhost';
 const port=3000;
 
 const app = express();
-app.use();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+    console.log(req.headers);
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/json');
+    res.end('<html><body><h1>This is my express server up on running</h1></body></html>');
+});
+
+const server=http.createServer(app);
+server.listen(port,hostname,()=>{
+    console.log(`The server is running on http://${hostname}:${port}`);
+})
