515b69d7766e6df557008611b18b31e735a9a2cf
Veena-02 <veenachaddha1602@outlook.com>
2021-03-18 20:36:00 +0530

    connecting node app to mongodb server

--- node-express/index.js
+++ node-express/index.js
@@ -3,49 +3,20 @@ const express=require('express');
 const morgan=require('morgan');
 const bodyParser=require('body-parser');
 
+const dishRouter = require('./routes/dishRouter.js')
+const promoRouter = require('./routes/promoRouter.js')
+const leaderRouter = require('./routes/leaderRouter.js')
+
 const hostname='localhost';
 const port=3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.json());
 
-app.all('/dishes',(req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type', 'text/html');
-    next();
-})
-app.get('/dishes',(req,res,next)=>{
-    res.end("Will send all the dishes to you!");
-})
-app.post('/dishes',(req,res,next)=>{
-    res.end("Will add the dish "+req.body.name+" with details: "+req.body.description);    
-})
-app.put('/dishes',(req,res,next)=>{
-    res.statusCode=403;
-    res.end("PUT operation not supported on /dishes");
-})
-app.delete('/dishes',(req,res,next)=>{
-    res.end("Deleting all dishes");
-})
-
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-})
-app.post('/dishes/:dishId',(req,res,next)=>{
-    res.statusCode=403;
-    res.end('Post operation not supported on /dishes/'+req.params.dishId);
-})
-app.put('/dishes/:dishId',(req,res,next)=>{
-    res.write('Updating the dish '+req.params.dishId+'\n');
-    res.end('Will update the dish: '+req.body.name+' with details: '+req.body.description);
-})
-app.delete('/dishes/:dishId',(req,res,next)=>{
-    res.end('Deleting the dish: ' + req.params.dishId );
-})
-
-
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 app.use((req,res,next)=>{
     console.log(req.headers);
@@ -59,3 +30,15 @@ const server=http.createServer(app);
 server.listen(port,hostname,()=>{
     console.log(`The server is running on http://${hostname}:${port}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ node-express/routes/leaderRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req,res,next)=>{
+   res.statusCode=200;
+   res.setHeader('Content-Type', 'text/html');
+   next();
+})
+.get((req,res,next)=>{
+   res.end("Will send all the leaders to you!");
+})
+.post((req,res,next)=>{
+   res.end("Will add the leader "+req.body.name+" with details: "+req.body.description);    
+})
+.put((req,res,next)=>{
+   res.statusCode=403;
+   res.end("PUT operation not supported on /leaders");
+})
+.delete((req,res,next)=>{
+   res.end("Deleting all leaders");
+})
+
+leaderRouter.route('/:leaderId')
+.get((req,res,next)=>{
+   res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+})
+.post((req,res,next)=>{
+   res.statusCode=403;
+   res.end('Post operation not supported on /leaders/:leaderId');
+})
+.put((req,res,next)=>{
+   res.write('Updating the leader '+req.params.leaderId+'\n');
+   res.end('Will update the leader: '+req.body.name+' with details: '+req.body.description);
+})
+.delete((req,res,next)=>{
+   res.end('Deleting the leader: ' + req.params.leaderId );
+})
+module.exports = leaderRouter;
